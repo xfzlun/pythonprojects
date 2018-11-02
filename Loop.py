54a49fcd09b057a35cc12599f72c481b1d9cd71f
xfzlun <1020976137@qq.com>
2018-11-02 11:22:31 +0800

    push error fixed

---
+++ Loop.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# _*_ coding: utf-8 _*_
+'''
+#@Time  :2018-10-09;22:09
+@author: aLuren
+
+#@FileName: Loop.py
+#@Software: PyCharm
+Copyright(C)2018 SZ-MB-QTC
+'''
+
+a =1
+while a<10:
+    print(a)
+    a+=2
